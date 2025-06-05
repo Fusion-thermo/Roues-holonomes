ab96318394e8eab2e66bb735900e5bc3901ca9f5
Jean BERTHAUD <jean.berthaud@outlook.com>
2025-06-05 18:08:12 +0200

    Update Roues holonomes.py

--- Roues holonomes.py
+++ Roues holonomes.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk #attention à l'ordre d'import car tkinter contient aussi Image
 import numpy as np
-from math import cos, sin, sqrt, pi
+from math import cos, sin, sqrt, pi, asin
 
 def rad(degre):
     return pi*degre/180
@@ -44,13 +44,20 @@ plateau = Canevas.create_image( 0, 0, image = img, anchor = "nw")
 #Robot avec polygone, vraies dimensions
 # Construction autour de 0,0
 r = (275+64/2)/sqrt(3) #rayon en mm
-r = r*1 #rayon en pixels
-robot = Canevas.create_polygon(r*cos(rad(10.4)), r*sin(rad(10.4)), r*cos(rad(109.6)), r*sin(rad(109.6)), r*cos(rad(130.4)), r*sin(rad(130.4)), r*cos(rad(-130.4)), r*sin(rad(-130.4)), r*cos(rad(-109.6)), r*sin(rad(-109.6)), r*cos(rad(-10.4)), r*sin(rad(-10.4)), fill="grey")
-#Position et orientation correcte
+r_inte = r-26 #inte et exte font référence à diamètre des sommets des roues, avec r_exte = r
+angle = asin(32/r)*180/pi
+angle_exte = asin(29/r)*180/pi
+angle_inte = asin(29/r_inte)*180/pi
+r *= 873/3000 #rayon en pixels
+r_inte *= 873/3000 #rayon en pixels
+x0, y0 = 99, 338
+robot = Canevas.create_polygon(r*cos(rad(angle)) + x0, r*sin(rad(angle)) + y0, r*cos(rad(120-angle)) + x0, r*sin(rad(120-angle)) + y0, r*cos(rad(120+angle)) + x0, r*sin(rad(120+angle)) + y0, r*cos(rad(-(120+angle))) + x0, r*sin(rad(-(120+angle))) + y0, r*cos(rad(-(120-angle))) + x0, r*sin(rad(-(120-angle))) + y0, r*cos(rad(-angle)) + x0, r*sin(rad(-angle)) + y0, fill="black")
+roue_avant = Canevas.create_rectangle(r_inte*cos(rad(angle_inte)) + x0, r_inte*sin(rad(angle_inte)) + y0, r*cos(rad(-angle_exte)) + x0, r*sin(rad(-angle_exte)) + y0, fill="red")
+roue_avant = Canevas.create_rectangle(r_inte*cos(rad(120-angle_inte)) + x0, r_inte*sin(rad(120-angle_inte)) + y0, r*cos(rad(120+angle_exte)) + x0, r*sin(rad(120+angle_exte)) + y0, fill="blue")
+
+
 
 Canevas.bind('<Button-1>',  Clic_gauche)
-
-
 
 # deplacement()
 
